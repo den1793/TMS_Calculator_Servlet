e088b18bfa682d61dd603986c9432ce5f21d6878
Denis Smirnov <den170193@yandex.ru>
2023-03-27 16:44:21 +0300

    Calculator Servlet

--- src/main/java/servlet/CalculatorServlet.java
+++ src/main/java/servlet/CalculatorServlet.java
@@ -15,20 +15,20 @@ import java.io.IOException;
  */
 @WebServlet(value = "/calc", name = "CalculatorServlet")
 public class CalculatorServlet extends HttpServlet {
-    CalculatorService calculatorService = new CalculatorService();
+    private final CalculatorService calculatorService = new CalculatorService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String sNum1 = req.getParameter("num1");
         String sNum2 = req.getParameter("num2");
-        String type = req.getParameter("type").toUpperCase();
+        String type = req.getParameter("operation-type").toUpperCase();
 
         double num1 = Double.parseDouble(sNum1);
         double num2 = Double.parseDouble(sNum2);
 
-        Operation operation = new Operation(num1,num2,type);
-        Operation result = calculatorService.calculate(operation);
+        Operation operation = calculatorService.calculate(new Operation(num1,num2,type));
 
-        resp.getWriter().print("Your result = " + result.getResult());
+
+        resp.getWriter().print(operation.getResult());
 
 
 
